65dd27991cb34d7998d5edfb448f861986249e36
Monudhakad1 <monudhakad055@gmail.com>
2026-02-19 10:05:53 +0530

    Spring.io

---
+++ src/main/java/com/authnexus/centralapplication/AuthNexusApplication.java
@@ -0,0 +1,13 @@
+package com.authnexus.centralapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthNexusApplication.class, args);
+    }
+
+}
