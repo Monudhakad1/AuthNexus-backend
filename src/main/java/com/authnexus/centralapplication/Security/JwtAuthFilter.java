3ab0a0d3b67fe344f0d243f834746f4da4dd6294
Monudhakad1 <monudhakad055@gmail.com>
2026-03-08 18:43:07 +0530

    Updated JWT services

---
+++ src/main/java/com/authnexus/centralapplication/Security/JwtAuthFilter.java
@@ -0,0 +1,108 @@
+package com.authnexus.centralapplication.Security;
+
+import com.authnexus.centralapplication.Helper.UserHelper;
+import com.authnexus.centralapplication.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtServices jwtService;
+    private final UserRepository userRepository;
+    Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+        log.info("Authorization header: {}", header);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+
+            if (!jwtService.isAccessToken(token)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            Jws<Claims> parsedToken = jwtService.parse(token);
+            Claims claims = parsedToken.getPayload();
+
+            String userId = claims.getSubject();
+            UUID userUuid = UserHelper.parseUUID(userId);
+
+            userRepository.findById(userUuid).ifPresent(user -> {
+
+                if (user.isEnable()
+                        && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                    List<GrantedAuthority> authorities =
+                            user.getRoles() == null
+                                    ? List.of()
+                                    : user.getRoles().stream()
+                                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                                    .collect(Collectors.toList());
+
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    user,
+                                    null,
+                                    authorities
+                            );
+
+                    authentication.setDetails(
+                            new WebAuthenticationDetailsSource()
+                                    .buildDetails(request)
+                    );
+
+                    SecurityContextHolder.getContext()
+                            .setAuthentication(authentication);
+                }
+            });
+
+        } catch (ExpiredJwtException e) {
+            log.warn("JWT token expired");
+        } catch (Exception e) {
+            log.warn("Invalid JWT token");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/api/v1/auth");
+    }
+}
--- src/main/java/com/authnexus/centralapplication/config/SecurityConfig.java
+++ src/main/java/com/authnexus/centralapplication/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.authnexus.centralapplication.config;
 
-import com.authnexus.centralapplication.domains.entities.User;
+import com.authnexus.centralapplication.Security.JwtAuthFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,29 +10,57 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+@RequiredArgsConstructor
+public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(
-                        authorizeRequests -> authorizeRequests
-                                .requestMatchers(HttpMethod.POST,"/api/v1/auth/register").permitAll()
-                                .requestMatchers("/api/v1/auth/login").permitAll()
-                                .anyRequest().authenticated()).
-                httpBasic(Customizer.withDefaults());
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex ->
+                        ex
+                                .authenticationEntryPoint((request, response, authException) -> {
+                                    response.setContentType("application/json");
+                                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                                    response.getWriter().write(
+                                            "{\"error\": \"Unauthorized\"}"
+                                    );
+                                })
+                                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                                    response.setContentType("application/json");
+                                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                                    response.getWriter().write(
+                                            "{\"error\": \"Forbidden\"}"
+                                    );
+                                })
+                )
+                // add jwt filter before username password authentication filter
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
